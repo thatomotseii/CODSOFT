c019ece8a6576cee0d9982726440c41c44237718
ThatoMotseki <thatomotseki080@gmail.com>
2024-10-02 09:07:18 +0200

    CodSoft initial Files

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job Listings - Home</title>
+    <link href="Styles/styles.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></head>
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item active">
+          <a class="nav-link active" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">Browse Jobs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">Pages</a>
+        </li>
+      </ul>
+      <a class="navbar-brand mx-auto" href="index.php">
+      <img src="Assets/Teamwork.svg" alt="Navigation Bar Brand" style="max-height: 40px;">
+    </a>
+      
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">Contact Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">Log in</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">Post a Job</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+    <h1>Welcome to Job Board</h1>
+
+    <button class="find-job-button">Find A Job</button>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
